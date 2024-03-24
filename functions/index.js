2cceb5330a4ca35e37a92f3ff04ded2199e91c30
Andersson Cordoba <anderson.980114@gmail.com>
2024-03-23 20:08:40 -0500

    prod config

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,20 @@
 import "dotenv/config";
-import './Database/MongoDb.js';
+import '../Database/MongoDb.js';
 
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import serverless from "serverless-http";
+
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swaggerConfig.js';  
+import swaggerSpec from '../swaggerConfig.js';  
 import fileUpload from 'express-fileupload'
 
-import productRoutes from './Routes/productRoutes.js'
-import brandRoutes from './Routes/brandRoutes.js'
-import clientRoutes from './Routes/clientRoutes.js' 
-import salesRoutes from './Routes/salesRoutes.js' 
-import priceRoutes from './Routes/priceRoutes.js' 
+import productRoutes from '../Routes/productRoutes.js'
+import brandRoutes from '../Routes/brandRoutes.js'
+import clientRoutes from '../Routes/clientRoutes.js' 
+import salesRoutes from '../Routes/salesRoutes.js' 
+import priceRoutes from '../Routes/priceRoutes.js' 
 
 const app = express();
 
@@ -31,11 +33,11 @@ app.use(morgan('dev'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Rutas
-app.use('/products', productRoutes);  
-app.use('/brands', brandRoutes);  
-app.use('/clients', clientRoutes);  
-app.use('/sale', salesRoutes);  
-app.use('/price', priceRoutes);  
+app.use('/.netlify/functions/index/products', productRoutes);  
+app.use('/.netlify/functions/index/brands', brandRoutes);  
+app.use('/.netlify/functions/index/clients', clientRoutes);  
+app.use('/.netlify/functions/index/sale', salesRoutes);  
+app.use('/.netlify/functions/index/price', priceRoutes);  
 
 // Configuración de CORS para permitir acceso desde tu frontend de React
 const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
@@ -52,9 +54,10 @@ app.use(cors(corsOptions));
 
 // Declarar los puertos
 const PORT = process.env.PORT || 3001;
- 
-app.listen(PORT, ()=> { 
+export const handler = serverless(app) 
+
+/* app.listen(PORT, ()=> { 
         console.log(`Servidor Express escuchando en el puerto ${PORT}`);
         console.log("ruta: http://localhost:"+ PORT)
     }
-)
+) */
